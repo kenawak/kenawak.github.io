dbbac576d20497d7c98cb4d53b91cc36ce7a68c7
KEN <74878728+kenawak@users.noreply.github.com>
2023-10-15 17:55:36 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//Sticky header
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      })
+    }
+  });
+  let header = document.querySelector('.header');
+
+  header.classList.toggle('sticky', window.scrollY > 100);
+};
+const typed = select('.typed')
+if (typed) {
+  let typed_strings = typed.getAttribute('data-typed-items')
+  typed_strings = typed_strings.split(',')
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
+}
