e2cd85a64a75adceb0fc8c77f7a7c6340a0d15e5
KEN <74878728+kenawak@users.noreply.github.com>
2023-10-16 18:39:07 -1200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 //Sticky header
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
+let menuIcon = document.querySelector('.bx-menu');
+let navbar = document.querySelector('.navbar');
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active')
+  })
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active')
+  })
+}
 
 window.onscroll = () => {
-
   sections.forEach(sec => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
@@ -11,7 +28,7 @@ window.onscroll = () => {
     let id = sec.getAttribute('id');
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach(link => {
+      navLinks.forEach(links => {
         links.classList.remove('active');
         document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
       })
@@ -21,3 +38,10 @@ window.onscroll = () => {
 
   header.classList.toggle('sticky', window.scrollY > 100);
 };
+
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('.bx-x');
+};
+
+
